f6ed47340facc213b73527a7fe2801c9af0b4cfd
Deborah Dalonso <deborahdlns@gmail.com>
2023-05-12 11:17:19 -0300

    v1

---
+++ app/Http/Requests/User/UserStoreRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //Libera qualquer requiquisição
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string|required',
+            'email' => 'email|required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.string' => 'O nome deve ser uma string!'
+        ];
+    }
+}
