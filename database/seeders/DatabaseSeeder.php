f23a0d926a1e59714c4b20decd06088dd0c37edc
Deborah Dalonso <deborahdlns@gmail.com>
2023-05-17 16:48:08 -0300

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
-        $this->call(AddressTableSeeder::class);
+        
+        // $this->call(AddressTableSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,10 +16,25 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Katarina',
-            'email' => 'k@gmail.com',
-            'password' => bcrypt('123')
-        ]);
+        //Ja insere os 3 users fakes
+        // User::factory()->count(3)->create();
+
+        //Coloca os usuarios em uma variavel
+        // $user = User::factory()->count(3)->make();
+
+        //Da na mesma que o primeiro mas é meio obscuro para leitura
+        // User::factory(3)->create();
+
+        //Quando existe um relacionamento ja cria os dados preenchendo as duas tabelas relacionadas, has server pra um usuario que tem um endereço, 1 para 1
+        // User::factory()->count(3)->has(Address::factory())->create();
+
+        //Mesma coisa que o anterior, só que usa um metodo magico para chamar o factory de Address
+        User::factory()->count(3)->hasAddress()->create();
+
+        // User::create([
+        //     'name' => 'Katarina',
+        //     'email' => 'k@gmail.com',
+        //     'password' => bcrypt('123')
+        // ]);
     }
 }
